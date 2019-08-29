17b54ac3ac19b64ece1362f3d94ef825882253d0
Adenle Abiodun <adenleabbey@gmail.com>
2019-08-29 04:36:44 +0100

    css
    css added

--- src/index.js
+++ src/index.js
@@ -12,13 +12,37 @@ function mul(x, y) {
   return x * y;
 }
 
+function identityf(numb) {
+  return function () {
+    return numb;
+  }
+}
+
+function addf(x) {
+  return function (y) {
+    return x+=y;
+  }
+} 
+function liftf(x) {
+  return function(y) {
+    return function (z) {
+      return x(y, z);
+    }
+  } 
+}
+var three = identityf(3);
+// var addfunc = liftf(add);
+
 document.getElementById("app").innerHTML = `
 <h1>Fun with Functions!</h1>
 <div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  I use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-<div> Result of ADD is ${add(2,3)}</div>
-<div> Result of SUB is ${sub(2,3)}</div>
-<div> Result of MUL is ${mul(2,3)}</div>
+<div class="sections"> Result of <pre>${add}</pre> with x=3 and y=4 is ${add(3,4)}</div><br>
+<div class="sections"> Result of <pre>${sub}</pre> with x=3 and y=4 is ${sub(3,4)}</div><br>
+<div class="sections"> Result of <pre>${mul}</pre> with x=3 and y=4 is ${mul(3,4)}</div><br>
+<div class="sections"> Result of <pre>${identityf}</pre> with x=3 and y=4 is ${three()}</div><br>
+<div class="sections"> Result of <pre>${addf}</pre> with x=3 and y=4 is ${addf(3)(4)}</div><br>
+<div class="sections"> Result of <pre>${liftf}</pre> with x=mul(), y=3 and y=4 is ${liftf(mul)(3)(4)}</div><br>
 `;
